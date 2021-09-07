e1d2ff2335ef08096551edf955a8834b75e5c792
Ivanov Lev <ivanovlm@mer.ci.nsu.ru>
2021-09-07 16:35:32 +0200

    Skeleton

--- main.c
+++ main.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
 
-int main(void)
+int need_stop;
+
+int init()
 {
-    printf("HELLO!\n");
+    // parse_config();
+    // init_net();
+
+    return 0;
+}
+
+int loop()
+{
+    while (!need_stop)
+    {
+        // poll
+    }
+    return 0;
+}
+
+int finalize()
+{
+    // clear sockets
+    // free mem
+}
+
+int main(int argc, char ** argv)
+{
+    init();
+
+    loop();
+
+    finalize();
+
     return 0;
 }
