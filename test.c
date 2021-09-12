958e65610d88aad54912d292688b68a9fb8ec890
Agl28E <dmitrij.shkuratov.00@mail.ru>
2021-09-12 20:26:53 +0700

    add functions

--- test.c
+++ test.c
@@ -4,123 +4,126 @@
 #include <string.h>
 #include "libs/cJSON/cJSON.h"
 
-cJSON *read_json( const char *filename )
+cJSON *parse_file(const char *filename)
 {
-    FILE *f = NULL;
-    f = fopen(filename, "r");
+        cJSON *parsed = NULL;
+        FILE *file = NULL;
+        file = fopen(filename, "r");
+        if (file == NULL)
+        {
+                printf("file not found");
+        }
 
-    if (f == NULL)
-    {
-        printf("file don't open");
-        return NULL;
-    }
-    
-    fseek(f, 0L, SEEK_END);
-    size_t buffer_len = ftell(f);
-    fseek(f, 0L, SEEK_SET);
+        fseek(file, 0L, SEEK_END);
+        size_t buffer_len = ftell(file);
+        fseek(file, 0L, SEEK_SET);
 
-    char *buffer = malloc(buffer_len + 1);
-    fread(buffer, 1, buffer_len, f);
-    buffer[buffer_len] = '\0';
-   
-    fclose(f);
+        char *buffer = malloc(buffer_len + 1);
+        fread(buffer, 1, buffer_len, file);
+        buffer[buffer_len] = '\0';
 
-    cJSON *root = NULL;
-    root = cJSON_Parse(buffer);
+        fclose(file);
 
-    if(root == NULL)
-    {
-        printf("parse fail\n");
-    }
+        parsed = cJSON_Parse(buffer);
 
-    free(buffer);
-   
-    return root;
+        if(parsed == NULL)
+        {
+                printf("parse fail\n");
+        }
+
+        free(buffer);
+
+        return parsed;
 }
 
-struct item_t
+typedef struct
 {
-    uint16_t id;
-    char * name;
-    char * desc;
-    uint32_t price;
-    uint32_t count;
+        uint16_t id;
+        char * name;
+        char * desc;
+        uint32_t price;
+        uint32_t count;
+} item_t;
+
+typedef struct
+{
+        char * adress;
+        int port;
+        //list;
+        int need_stop;
+        int listen_socket;
+
+} server_t;
+
+int item_free(item_t * item)
+{
+        free(item->name);
+        free(item->desc);
+        return 0;
+}
+
+int item_parse(item_t * item, cJSON * cjson_items_item)
+{           
+        item->id =  cJSON_GetObjectItem(cjson_items_item, "id") -> valueint;
+        printf("id:%d\n", item->id);
+
+        item->name = (char*)malloc(strlen(cJSON_GetObjectItem(cjson_items_item, "name") -> valuestring) * sizeof(char));
+        strcpy(item->name, cJSON_GetObjectItem(cjson_items_item, "name") -> valuestring);
+        printf("name:%s\n", item->name);
+        
+        item->desc = (char*)malloc(strlen(cJSON_GetObjectItem(cjson_items_item, "desc") -> valuestring) * sizeof(char));
+        strcpy(item->desc, cJSON_GetObjectItem(cjson_items_item, "desc") -> valuestring);
+        printf("desc:%s\n", item->desc);
+        
+        item_free(item);
+
+        item->price = cJSON_GetObjectItem(cjson_items_item, "price") -> valueint;
+        printf("price:%d\n", item->price);
+
+        item->price = cJSON_GetObjectItem(cjson_items_item, "count") -> valueint;
+        printf("count:%d\n", item->price);
+        printf("\n");
+        return 0;
+}
+
+int items_parse(server_t * server, cJSON * cjson_parse)
+{
+        cJSON * cjson_items = NULL;
+        cJSON * cjson_items_item = NULL;
+        cjson_items = cJSON_GetObjectItem(cjson_parse, "items");
+        printf("items:\n");
+
+        item_t * item = malloc(sizeof(* item));
+        cJSON_ArrayForEach(cjson_items_item, cjson_items)
+        {
+                item_parse(item, cjson_items_item);
+        }
+        free(item);
+        //Взаимодействие с лист
+        return 0;       
 };
 
-int item_free(struct item_t * items)
+int config_parse(server_t * server)
 {
-    free(items->name);
-    free(items->desc);
-    return 0;
-}
+        cJSON* cjson_parse = NULL;
+        cJSON* cjson_address = NULL;
+        cJSON* cjson_port = NULL;
+        cjson_parse = parse_file("conf.json");
 
+        cjson_address = cJSON_GetObjectItem(cjson_parse, "address");
+        cjson_port = cJSON_GetObjectItem(cjson_parse, "port");
+
+        printf("address:%s\n", cjson_address->valuestring);
+        printf("port:%d\n", cjson_port->valueint);
+        items_parse(server, cjson_parse);
+        cJSON_Delete(cjson_parse);
+        
+        return 0;      
+};
 
 int main(void)
 {
-
-    struct item_t items[6]; 
-    
-
-    cJSON* cjson_test = NULL;
-    cJSON* cjson_address = NULL;
-    cJSON* cjson_port = NULL;
-    cJSON* cjson_items = NULL;
-    
-    cJSON* cjson_items_id = NULL;
-    cJSON* cjson_items_id_item = NULL;
-    cJSON* cjson_items_name = NULL;
-    cJSON* cjson_items_desc = NULL;
-    cJSON* cjson_items_price = NULL;
-    cJSON* cjson_items_count = NULL;
-    int    items_array_size = 0, i = 0;
-    cJSON* cjson_items_item = NULL;
-
-   
-    // * Анализировать все данные JSO * /
-    cjson_test = read_json("conf.json");
-    //cjson_test = cJSON_Parse(load_json("conf.json"));
-   
-    cjson_address = cJSON_GetObjectItem(cjson_test, "address");
-    cjson_port = cJSON_GetObjectItem(cjson_test, "port");
-
-     
-    printf("address:%s\n", cjson_address->valuestring);
-    printf("port:%d\n", cjson_port->valueint);
-
-   
-    cjson_items = cJSON_GetObjectItem(cjson_test, "items");
-    items_array_size = cJSON_GetArraySize(cjson_items);
-    printf("items:\n");
-    for(i = 0; i < items_array_size; i++) 
-    {
-        cjson_items_item = cJSON_GetArrayItem(cjson_items, i);
-        cjson_items_id = cJSON_GetObjectItem(cjson_items_item, "id");
-        cjson_items_name = cJSON_GetObjectItem(cjson_items_item, "name");
-        cjson_items_desc = cJSON_GetObjectItem(cjson_items_item, "desc");
-        cjson_items_price = cJSON_GetObjectItem(cjson_items_item, "price");
-        cjson_items_count = cJSON_GetObjectItem(cjson_items_item, "count");
-        
-        items[i].id = cjson_items_id -> valueint;
-        printf("id:%d\n", items[i].id);
-       
-        items[i].name = (char*)malloc(strlen(cjson_items_name -> valuestring) * sizeof(char));
-        strcpy(items[i].name, cjson_items_name -> valuestring);
-        printf("name:%s\n", items[i].name);
-        
-        items[i].desc = (char*)malloc(strlen(cjson_items_desc -> valuestring) * sizeof(char));
-        strcpy(items[i].desc, cjson_items_name -> valuestring);
-        printf("desc:%s\n", items[i].desc);
-        
-        item_free(&items[i]);
-
-        items[i].price = cjson_items_price -> valueint;
-        printf("price:%d\n", items[i].price);
-        
-        items[i].price = cjson_items_count -> valueint;
-        printf("count:%d\n", items[i].price);
-        printf("\n");
-    }
-    cJSON_Delete(cjson_test);
-    return 0;
+        server_t * server;
+        config_parse(server);
+        return 0;
 }
-
